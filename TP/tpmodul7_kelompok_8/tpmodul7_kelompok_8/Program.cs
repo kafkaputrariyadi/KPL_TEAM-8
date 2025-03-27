5f7f29f9dbaab04c0e273a023c483f92d451c1b5
Ahmad Al - Farizi <92077482+alxfarizi@users.noreply.github.com>
2025-03-28 00:10:07 +0700

    menambahkan class KuliahMahasiswa2311104054

--- TP/tpmodul7_kelompok_8/tpmodul7_kelompok_8/Program.cs
+++ TP/tpmodul7_kelompok_8/tpmodul7_kelompok_8/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+using tpmodul7_kelompok_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KuliahMahasiswa2311104054 kuliahMahasiswa = new KuliahMahasiswa2311104054();
+
+        kuliahMahasiswa.ReadJSON();
+    }
+}

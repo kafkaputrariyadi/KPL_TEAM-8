7350aea1833b721c9c3fddea5b93b3e40b1bd068
Naura03 <nauraaisha123@gmail.com>
2025-04-10 17:12:48 +0700

    menambahkan class DataMahasiswa2311104078

---
+++ TP/tpmodul7_kelompok_8/tpmodul7_kelompok_8/DataMahasiswa2311104078.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+class DataMahasiswa2311104078
+{
+    public string Nama { get; set; }
+    public string Nim { get; set; }
+    public string Fakultas { get; set; }
+
+    public static void ReadJSON()
+    {
+        string path = "tp7_1_2311104078.json"; 
+        string json = File.ReadAllText(path);
+
+        DataMahasiswa2311104078 data = JsonConvert.DeserializeObject<DataMahasiswa2311104078>(json);
+
+        Console.WriteLine($"Nama {data.Nama} dengan nim {data.Nim} dari fakultas {data.Fakultas}");
+    }
+}
--- TP/tpmodul7_kelompok_8/tpmodul7_kelompok_8/Program.cs
+++ TP/tpmodul7_kelompok_8/tpmodul7_kelompok_8/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa2311104078.ReadJSON();
+    }
+}
